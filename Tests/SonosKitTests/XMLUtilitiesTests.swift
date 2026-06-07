18579ae06d93725093e3c696047c11e26cbe6a55
Denver Trouton <dtrouton@gmail.com>
2026-06-07 01:48:40 +0100

    test: cover XML utilities; move getLocalIPAddress out

--- Tests/SonosKitTests/XMLUtilitiesTests.swift
+++ Tests/SonosKitTests/XMLUtilitiesTests.swift
@@ -1,8 +1,24 @@
 import XCTest
 @testable import SonosKit
 
-final class PackageSmokeTests: XCTestCase {
-    func testPackageBuilds() {
-        XCTAssertTrue(true)
+final class XMLUtilitiesTests: XCTestCase {
+    func testExtractsValueBetweenTags() {
+        XCTAssertEqual(extractXMLValue(from: "<roomName>Kitchen</roomName>", tag: "roomName"), "Kitchen")
+    }
+
+    func testReturnsNilForMissingTag() {
+        XCTAssertNil(extractXMLValue(from: "<a>x</a>", tag: "b"))
+    }
+
+    func testReturnsNilForEmptyValue() {
+        XCTAssertNil(extractXMLValue(from: "<a></a>", tag: "a"))
+    }
+
+    func testEscapeReplacesEntities() {
+        XCTAssertEqual(xmlEscape("a & b < c > d \" e"), "a &amp; b &lt; c &gt; d &quot; e")
+    }
+
+    func testUnescapeReversesEntities() {
+        XCTAssertEqual(xmlUnescape("a &amp; b &lt; c &gt; d &quot; e &apos; f"), "a & b < c > d \" e ' f")
     }
 }
